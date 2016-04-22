5e479dc632fcd327ab2a9f81ecc8ecbef49bf8c2
Alexandre Courouble <couralex6@gmail.com>
2016-04-21 20:44:01 -0400

    changed IP

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 cnx = mysql.connector.connect(user='root', password='root',
-                              host='172.31.30.75',
+                              host='172.31.30.76',
                               database='sakila')
 
 
