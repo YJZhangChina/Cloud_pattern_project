942bec60934ef5c114acb04427c13fa0b0b84825
Alexandre Courouble <couralex6@gmail.com>
2016-04-22 17:13:58 -0400

    getting there

--- file.py
+++ file.py
@@ -25,17 +25,6 @@ cnx = mysql.connector.connect(user=USER, password=PASSWORD,
                               host=HOST,
                               database=DB)
 
-def readQuery(query, nb):
-	cursor = cnx.cursor()
-	timeA = time.time()
-	print "executing: ", query, " for ", nb, "times"
-	for i in range(nb):
-		cursor.execute(query)
-		row = cursor.fetchall()	
-	cursor.close()
-	return time.time() - timeA 
-
-
 def writeQuery(nb):
 	cursor = cnx.cursor()
 	timeA = time.time()
@@ -50,12 +39,24 @@ def writeQuery(nb):
 	return time.time() - timeA 
 
 
-#latency = readQuery("SELECT * FROM film;", 1000)
-#print "Latency: 		", latency
+def readQuery(query, nb):
+	cursor = cnx.cursor()
+	timeA = time.time()
+	print "executing: ", query, " for ", nb, "times"
+	for i in range(nb):
+		cursor.execute(query)
+		row = cursor.fetchall()	
+	cursor.close()
+	return time.time() - timeA 
+
+
+
 
 latencyWrite = writeQuery(100)
-
+latencyRead = readQuery("SELECT * FROM test;", 1000)
 print latencyWrite
+print "Latency: 		", latency
+
 
 
 cnx.close()
