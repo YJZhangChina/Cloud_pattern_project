87a16ed9b785094207ebe1037107b3e673214733
Alexandre Courouble <couralex6@gmail.com>
2016-04-25 15:03:31 -0400

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,2 +1,8 @@
 import subprocess
 
+def bashCommand(command_str):
+    cmd = subprocess.Popen(command_str.split(' '), stdout=subprocess.PIPE)
+    cmd_out, cmd_err = cmd.communicate()
+    return cmd_out
+
+print bashCommand('sysbench  --test=oltp --num-threads=32 --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster  --mysql-db=sakila run')
