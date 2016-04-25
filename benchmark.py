b8ef4a788a140b49aedad5a943dfcfa8b153f188
Alexandre Courouble <couralex6@gmail.com>
2016-04-25 17:54:25 -0400

    multiple requests now implemented

--- benchmark.py
+++ benchmark.py
@@ -8,6 +8,8 @@ benchMark for STANDALONE DB
 
 threads = [1,2,4,8,16,32,64]
 
+requests = [5000, 10000]
+
 theFile = open("output.txt", "w")
 
 def printToFile(toPrint, i):
@@ -22,8 +24,9 @@ def bashCommand(command_str, i):
     printToFile(cmd_out, i)
 for j in range(5):
 	theFile.write("=====================================   RUN " + str(j+1) + " ======================================")
-	for i in threads:
-		string = "sysbench --test=oltp --num-threads=" + str(i) + " --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run"
-		bashCommand(string,i)
+	for k in requests:
+		for i in threads:
+			string = "sysbench --test=oltp --num-threads=" + str(i) + " --max-requests=" + str(k) + " --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run"
+			bashCommand(string,i)
 
 theFile.close()
