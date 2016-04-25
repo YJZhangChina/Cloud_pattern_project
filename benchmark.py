fe1694619545f5121e5fd705a39e500f705584ba
Alexandre Courouble <couralex6@gmail.com>
2016-04-25 16:44:42 -0400

    first test

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,22 @@
 import subprocess
 
-def bashCommand(command_str):
+threads = [1,2,4,8,16,32,64]
+
+theFile = open("output.txt", "w")
+
+def printToFile(toPrint, i):
+	theFile.write(str(i))
+	theFile.write("\n")
+	theFile.write(toPrint)
+	theFile.write("\n")	
+
+def bashCommand(command_str, i):
     cmd = subprocess.Popen(command_str.split(' '), stdout=subprocess.PIPE)
     cmd_out, cmd_err = cmd.communicate()
-    return cmd_out
+    printToFile(cmd_out, i)
 
-print bashCommand('sysbench --test=oltp --num-threads=32 --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run')
+for i in threads:
+	string = "sysbench  --test=oltp --num-threads=" + str(i) + " --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password= --mysql-table-engine=ndbcluster --mysql-db=sakila --mysql-socket=/tmp/mysql.sock run"
+	bashCommand(string,i)
+
+theFile.close()
