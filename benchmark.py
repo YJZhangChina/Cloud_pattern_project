954f448d301096a4d15e7d3f4a2ae19e1698038d
Alexandre Courouble <couralex6@gmail.com>
2016-04-25 16:59:19 -0400

    formatting

--- benchmark.py
+++ benchmark.py
@@ -20,9 +20,10 @@ def bashCommand(command_str, i):
     cmd = subprocess.Popen(command_str.split(' '), stdout=subprocess.PIPE)
     cmd_out, cmd_err = cmd.communicate()
     printToFile(cmd_out, i)
-
-for i in threads:
-	string = "sysbench --test=oltp --num-threads=" + str(i) + " --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run"
-	bashCommand(string,i)
+for j in range(5);
+	theFile.write("=====================================   RUN ", str(j+1), " ======================================")
+	for i in threads:
+		string = "sysbench --test=oltp --num-threads=" + str(i) + " --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run"
+		bashCommand(string,i)
 
 theFile.close()
