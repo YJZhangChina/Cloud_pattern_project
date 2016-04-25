533d63b40c1d34ba21a140811037b0618617671c
Alexandre Courouble <couralex6@gmail.com>
2016-04-25 17:02:23 -0400

    bug fix

--- benchmark.py
+++ benchmark.py
@@ -21,7 +21,7 @@ def bashCommand(command_str, i):
     cmd_out, cmd_err = cmd.communicate()
     printToFile(cmd_out, i)
 for j in range(5):
-	theFile.write("=====================================   RUN ", str(j+1), " ======================================")
+	theFile.write("=====================================   RUN " + str(j+1) + " ======================================")
 	for i in threads:
 		string = "sysbench --test=oltp --num-threads=" + str(i) + " --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run"
 		bashCommand(string,i)
