78631366fcebca20c4fc2a106c1bb65323fc1a12
Alexandre Courouble <couralex6@gmail.com>
2016-04-25 15:26:12 -0400

    bug fix

--- benchmark.py
+++ benchmark.py
@@ -5,4 +5,4 @@ def bashCommand(command_str):
     cmd_out, cmd_err = cmd.communicate()
     return cmd_out
 
-print bashCommand('sysbench  --test=oltp --num-threads=32 --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster  --mysql-db=sakila run')
+print bashCommand('sysbench  --test=oltp --num-threads=32 --max-requests=10000 --db-driver=mysql --mysql-user=root --mysql-password=root --mysql-table-engine=ndbcluster --mysql-db=sakila run')
